24ee073460e8592274c5782b8b3bc31879f1d18c
J-Agens <johntagens@gmail.com>
2019-03-24 12:12:32 -0400

    done

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,6 +1,8 @@
 class Network < ActiveRecord::Base
   has_many :shows
-
+  # ADDED MYSELF
+  has_many :characters, through: :shows
+  has_many :actors, through: :shows
   def sorry
     "We're sorry about passing on John Mulaney's pilot"
   end
